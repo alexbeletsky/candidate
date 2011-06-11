aa6f20f6ae85d8ad3979d6e5f56c976647f847d9
alexander.beletsky <alexander.beletsky@gmail.com>
2011-06-11 21:31:04 +0300

    start to do clone

--- Candidate.Core/Settings/ISettingsManager.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Candidate.Core.Settings
-{
-    public interface ISettingsManager
-    {
-        T ReadSettings<T>() where T : new();
-        void SaveSettings(object settings);
-    }
-}
