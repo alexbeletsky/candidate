51e4bff4f4153b2c3dc5c10ba40f15ce6b5b88b3
alexander.beletsky <alexander.beletsky@gmail.com>
2011-08-31 12:11:32 +0300

    Added log controller and display of log at the end of build;

--- Candidate.Web/Areas/Dashboard/Controllers/LogController.cs
+++ Candidate.Web/Areas/Dashboard/Controllers/LogController.cs
@@ -11,28 +11,14 @@ namespace Candidate.Areas.Dashboard.Controllers {
             _directoryProvider = directoryProvider;
         }
 
-        //[HttpGet]
-        //public ActionResult ReadLog(string jobName, string logId, int offset) {
-        //    var bufferSize = 512;
-        //    var buffer = new char[bufferSize];
-        //    var count = 0;
-
-        //    using (var reader = new StreamReader(logId)) {
-        //        reader.BaseStream.Seek(offset, SeekOrigin.Begin);
-        //        count = reader.Read(buffer, 0, buffer.Length);
-        //    }
-
-        //    return Json(new { success = true, eof = count == 0, count = count, line = new string(buffer, 0, count) }, JsonRequestBehavior.AllowGet);
-        //}
-
         [HttpGet]
         public ActionResult ShowLog(string jobName, string logId) {
+            _directoryProvider.JobName = jobName;
 
             var pathToLog = _directoryProvider.Logs + "\\" + logId;
             using (var reader = new StreamReader(pathToLog)) {
                 return new ContentResult { ContentType = "text/plain", Content = reader.ReadToEnd() }; 
             }
-
         }
     }
 }
--- Candidate.Web/Areas/Dashboard/DashboardAreaRegistration.cs
+++ Candidate.Web/Areas/Dashboard/DashboardAreaRegistration.cs
@@ -15,6 +15,12 @@ namespace Candidate.Areas.Dashboard
         public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute(
+                "Dashboard_log",
+                "Dashboard/{controller}/{action}/{jobName}/{logId}",
+                new { controller = "Log" }
+            );
+
+            context.MapRoute(
                 "Dashboard_default",
                 "Dashboard/{controller}/{action}/{jobName}",
                 new { action = "Index", controller = "Dashboard", jobName = UrlParameter.Optional }
