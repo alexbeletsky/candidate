042ba627d2f3f9c691160209c6bf7072caa02d42
alexander.beletsky <alexander.beletsky@gmail.com>
2011-09-12 22:44:16 +0300

    removed commented code;

--- Candidate.Web/Scripts/Controllers/controller.configure.js
+++ Candidate.Web/Scripts/Controllers/controller.configure.js
@@ -7,41 +7,10 @@
 $(function () {
 
     var Configure = function () {
-
-        var dialogShown = false;
-        $('.dialog').hide();
-
         $('.configuration-box').live('click', function () {
-            //            var source = $(this).data('source');
-            //            $('.dialog').load(source, function () {
-
-            //                showDialog(this);
-            //                $(this).find('form').ajaxForm(function () {
-            //                    closeDialog();
-            //                });
-            //            });
             var dialog = new Dialog($(this).data('source'));
             dialog.show();
 
         });
-
-        //        // helpers
-        //        function showDialog(d) {
-        //            dialogShown = true;
-        //            $.blockUI({ message: $(d), css: { width: '640px', padding: '0 0 8px 0', textAlign: 'left'} });
-        //        }
-
-        //        function closeDialog() {
-        //            if (dialogShown) {
-        //                $.unblockUI();
-        //            }
-        //        }
-
-        //        // global key handers
-        //        $(document).keyup(function (e) {
-        //            if (e.keyCode == 27) {
-        //                closeDialog();
-        //            }
-        //        });
     } ();
 });
