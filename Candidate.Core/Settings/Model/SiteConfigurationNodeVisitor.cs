96076514e39045e70ec8602ce0a8fbb7b8da298a
alexander.beletsky <alexander.beletsky@gmail.com>
2011-09-26 19:49:28 +0300

    Some code style and typos;

--- Candidate.Core/Settings/Model/SiteConfigurationNodeVisitor.cs
+++ Candidate.Core/Settings/Model/SiteConfigurationNodeVisitor.cs
@@ -6,31 +6,31 @@
     public abstract class SiteConfigurationNodeVisitor
     {
         /// <summary>
-        /// Perfoms SiteConfiguration node specific actions
+        /// Performs SiteConfiguration node specific actions
         /// </summary>
         /// <param name="node">The SiteConfiguration node.</param>
         public abstract void Visit(SiteConfiguration node);
 
         /// <summary>
-        /// Perfoms GitHub node specific actions
+        /// Performs GitHub node specific actions
         /// </summary>
         /// <param name="node">The GitHub node.</param>
         public abstract void Visit(GitHub node);
 
         /// <summary>
-        /// Perfoms Solution node specific actions
+        /// Performs Solution node specific actions
         /// </summary>
         /// <param name="node">The Solution node.</param>
         public abstract void Visit(Solution node);
 
         /// <summary>
-        /// Perfoms Iis node specific actions
+        /// Performs Iis node specific actions
         /// </summary>
         /// <param name="node">The Iis node.</param>
         public abstract void Visit(Iis node);
 
         /// <summary>
-        /// Perfoms Post node specific actions
+        /// Performs Post node specific actions
         /// </summary>
         /// <param name="node">The Post node.</param>
         public abstract void Visit(Post node);
--- Candidate.Core/Setup/ConfigObjectBuilder.cs
+++ Candidate.Core/Setup/ConfigObjectBuilder.cs
@@ -3,25 +3,26 @@ using Candidate.Core.Settings.Model;
 using Candidate.Core.Utils;
 
 namespace Candidate.Core.Setup {
-    public class ConfigObjectBuilder : IConfigObjectBuilder 
-    {
+    public class ConfigObjectBuilder : IConfigObjectBuilder {
         private readonly IDirectoryProvider _directoryProvider;
 
         public ConfigObjectBuilder(IDirectoryProvider directoryProvider) {
             _directoryProvider = directoryProvider;
         }
 
-        // TODO: hard coded values as framework version, target have to be moved to build configuration
-        public ConfigObject CreateConfigObject(SiteConfiguration siteConfiguration)
-        {
-            if (siteConfiguration == null)
-            {
+        public ConfigObject CreateConfigObject(SiteConfiguration siteConfiguration) {
+            if (siteConfiguration == null) {
                 throw new ArgumentNullException("siteConfiguration");
             }
 
-            var visitor = new ConfigObjectCreatingNodeVisitor(_directoryProvider);
-            siteConfiguration.Accept(visitor);
-            return visitor.ConfigObject;
+            siteConfiguration.Accept(ConfigVisitor);
+            return ConfigVisitor.ConfigObject;
+        }
+
+        public ConfigObjectCreatingNodeVisitor ConfigVisitor {
+            get {
+                return new ConfigObjectCreatingNodeVisitor(_directoryProvider);
+            }
         }
     }
 }
