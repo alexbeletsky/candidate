869971fa70767a9b016bfe6384a686135b0071a0
alexander.beletsky <alexander.beletsky@gmail.com>
2011-10-06 12:40:04 +0300

    Fixed the Dashboard/List buttons issue in Firefox/IE. Now using javascript for changing location;

--- Candidate.Web/Scripts/Controllers/controller.dashboard.js
+++ Candidate.Web/Scripts/Controllers/controller.dashboard.js
@@ -6,47 +6,59 @@ $(function () {
 
         $('.loader').hide();
 
-        $('#add-job-button').live('click', function () {
-            $('html, body').css('cursor', 'progress');
-            $('.loader').show();
+        (function addNewJobFunction() {
+            $('#add-job-button').live('click', function () {
+                $('html, body').css('cursor', 'progress');
+                $('.loader').show();
 
-            var method = $(this).data('method');
-            $('#action-content').slideUp().load(method, function () {
-                $(this).slideDown(function () {
-                    $('html, body').animate({ scrollTop: $('#action-content').offset().top }, 700);
-                    $(this).find('#Name').focus();
-                    $('html, body').css('cursor', 'auto');
-                    $('.loader').hide();
+                var method = $(this).data('method');
+                $('#action-content').slideUp().load(method, function () {
+                    $(this).slideDown(function () {
+                        $('html, body').animate({ scrollTop: $('#action-content').offset().top }, 700);
+                        $(this).find('#Name').focus();
+                        $('html, body').css('cursor', 'auto');
+                        $('.loader').hide();
+                    });
                 });
+
+                return false;
             });
 
-            return false;
-        });
-
-        $('#close-button').live('click', function () {
-            $('#action-content').slideUp(function () {
-                $(this).empty();
-            });
-
-            return false;
-        });
-
-        $('#add-job input').live('keyup', function (e) {
-            if (e.keyCode == 13) {
-                $('#add-job button[type=submit]').click();
-                e.preventDefault();
-            }
-        });
-
-        var temporaryPassword = $('#temporaryPassword').val();
-        if (temporaryPassword === "True") {
-            $('li#account').after('<div class="tooltip"><strong>Please create own user</strong>. You are now using account with temporary password, consider to change it as soon as possible.</div>');
-            setTimeout(function () {
-                $('.tooltip').fadeOut(2000, function () {
+            $('#close-button').live('click', function () {
+                $('#action-content').slideUp(function () {
                     $(this).empty();
                 });
-            }, 7000);
-        }
+
+                return false;
+            });
+
+            $('#add-job input').live('keyup', function (e) {
+                if (e.keyCode == 13) {
+                    $('#add-job button[type=submit]').click();
+                    e.preventDefault();
+                }
+            });
+
+        })();
+
+        (function temporaryPasswordReminderTooltip() {
+            var temporaryPassword = $('#temporaryPassword').val();
+            if (temporaryPassword === "True") {
+                $('li#account').after('<div class="tooltip"><strong>Please create own user</strong>. You are now using account with temporary password, consider to change it as soon as possible.</div>');
+                setTimeout(function () {
+                    $('.tooltip').fadeOut(2000, function () {
+                        $(this).empty();
+                    });
+                }, 7000);
+            }
+        })();
+
+
+        (function sitesButtons() {
+            $('#buttons button').live('click', function () {
+                window.location = $(this).data('target');
+            });
+        })();
 
     } ();
 });
