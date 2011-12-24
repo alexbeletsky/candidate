786637f592f7baecb072864812ad8e861d9bc558
alexander.beletsky <alexander.beletsky@gmail.com>
2011-12-24 22:32:28 +0200

    Removed unused code

--- Candidate.Web/Areas/Configuration/Controllers/ConfigurationController.cs
+++ Candidate.Web/Areas/Configuration/Controllers/ConfigurationController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Linq;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Candidate.Areas.Dashboard.Models;
 using Candidate.Core.Configurations;
-using Candidate.Core.Model;
 using Candidate.Core.Settings;
 using Candidate.Infrustructure.Filters;
 using Candidate.Core.Extensions;
@@ -73,127 +70,6 @@ namespace Candidate.Areas.Configuration.Controllers
             }
 
             return View();
-        }
-
-
-        [HttpGet, AddViewNameAndHash]
-        public ActionResult Pre(string id, Config.ConfigurationType type)
-        {
-            throw new NotImplementedException();
-        }
-
-        [HttpPost]
-        public ActionResult Pre(string id, Pre config)
-        {
-            throw new NotImplementedException();
-
-            //if (ModelState.IsValid)
-            //{
-            //    _settingsManager.SaveSiteConfiguration(id, c => c.Pre = config);
-            //    return Json(new { success = true });
-            //}
-
-            //return Json(new { success = false });
-        }
-
-        [HttpGet, AddViewNameAndHash]
-        public ActionResult Github(string id)
-        {
-            throw new NotImplementedException();
-
-            //var currentConfiguration = _settingsManager.ReadSettings<ConfigurationsList>();
-            //var siteConfiguration = currentConfiguration.Configurations.Where(c => c.Id == id).SingleOrDefault();
-
-            //return View(siteConfiguration.Github);
-        }
-
-        [HttpPost]
-        public ActionResult Github(string id, Github config)
-        {
-            throw new NotImplementedException();
-
-            //if (ModelState.IsValid)
-            //{
-            //    _settingsManager.SaveSiteConfiguration(id, c => c.Github = config);
-            //    return Json(new { success = true });
-            //}
-
-            //return Json(new { success = false });
-        }
-
-        [HttpGet, AddViewNameAndHash]
-        public ActionResult Iis(string id)
-        {
-            throw new NotImplementedException();
-
-            //var currentConfiguration = _settingsManager.ReadSettings<ConfigurationsList>();
-            //var siteConfiguration = currentConfiguration.Configurations.Where(c => c.Id == id).SingleOrDefault();
-
-            //return View(siteConfiguration.Iis ?? new Iis());
-        }
-
-        [HttpPost]
-        public ActionResult Iis(string id, Iis config)
-        {
-            throw new NotImplementedException();
-
-            //if (ModelState.IsValid)
-            //{
-            //    _settingsManager.SaveSiteConfiguration(id, c => c.Iis = config);
-            //    return Json(new { success = true });
-            //}
-
-            //return Json(new { success = false });
-        }
-
-        [HttpGet, AddViewNameAndHash]
-        public ActionResult Solution(string id)
-        {
-            throw new NotImplementedException();
-
-            //var currentConfiguration = _settingsManager.ReadSettings<ConfigurationsList>();
-            //var siteConfiguration = currentConfiguration.Configurations.Where(c => c.Id == id).SingleOrDefault();
-
-            //return View(siteConfiguration.Solution ?? new Solution());
-        }
-
-        [HttpPost]
-        public ActionResult Solution(string id, Solution config)
-        {
-            throw new NotImplementedException();
-
-            //if (ModelState.IsValid)
-            //{
-            //    _settingsManager.SaveSiteConfiguration(id, c => c.Solution = config);
-            //    return Json(new { success = true });
-            //}
-
-            //return Json(new { success = false });
-        }
-
-        [HttpGet, AddViewNameAndHash]
-        public ActionResult Post(string id)
-        {
-            throw new NotImplementedException();
-
-            //var currentConfiguration = _settingsManager.ReadSettings<ConfigurationsList>();
-            //var siteConfiguration = currentConfiguration.Configurations.Where(c => c.Id == id).SingleOrDefault();
-
-            //return View(siteConfiguration.Post);
-        }
-
-        [HttpPost]
-        public ActionResult Post(string id, Post config)
-        {
-            throw new NotImplementedException();
-
-            //if (ModelState.IsValid)
-            //{
-            //    _settingsManager.SaveSiteConfiguration(id, c => c.Post = config);
-            //    return Json(new { success = true });
-            //}
-
-            //return Json(new { success = false });
         }
     }
 }
