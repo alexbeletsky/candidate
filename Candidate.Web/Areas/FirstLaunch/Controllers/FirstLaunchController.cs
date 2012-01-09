6d3351137f91afb23e3a9edb73f0793039bfce7f
alexander.beletsky <alexander.beletsky@gmail.com>
2012-01-09 19:30:21 +0200

    added view for first launch

---
+++ Candidate.Web/Areas/FirstLaunch/Controllers/FirstLaunchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Candidate.Areas.FirstLaunch.Controllers
+{
+    public class FirstLaunchController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
