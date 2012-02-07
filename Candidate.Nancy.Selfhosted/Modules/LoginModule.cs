c5e169774b50a53691cae46cb11dbb7a489464c5
alexander.beletsky <alexander.beletsky@gmail.com>
2012-02-07 21:43:21 +0200

    nancy

---
+++ Candidate.Nancy.Selfhosted/Modules/LoginModule.cs
@@ -0,0 +1,20 @@
+﻿using Nancy;
+
+namespace Candidate.Nancy.Selfhosted.Modules
+{
+    public class LoginModule : NancyModule
+    {
+        public LoginModule()
+        {
+            Get["/"] = x => { return View["Login"]; };
+        }
+    }
+
+    public class SuperModule : NancyModule
+    {
+        public SuperModule()
+        {
+            Get["/super"] = _ => { return View["super"]; };
+        }
+    }
+}
---
+++ Candidate.Nancy.Selfhosted/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nancy.Hosting.Self;
+
+namespace Candidate.Nancy.Selfhosted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new NancyHost(new Uri("http://localhost:1111"));
+            host.Start(); // start hosting
+
+            Console.ReadKey();
+            host.Stop();
+        }
+    }
+}
