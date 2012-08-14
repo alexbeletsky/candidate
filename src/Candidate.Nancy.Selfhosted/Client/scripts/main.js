ba982f5e2b87f7f14e1fcf0603755bc75cf52cb9
alexander.beletsky <alexander.beletsky@gmail.com>
2012-08-14 10:37:34 +0300

    prepearing infrastructure for requirejs

---
+++ src/Candidate.Nancy.Selfhosted/Client/scripts/main.js
@@ -0,0 +1,22 @@
+﻿require.config({
+    shim: {
+        'Underscore': {
+            exports: '_'
+        },
+
+        'Backbone': {
+            deps: ['Underscore', 'jQuery'],
+            exports: 'Backbone'
+        }
+    },
+
+    paths: {
+        Backbone: 'libs/backbone',
+        jQuery: 'libs/jquery-1.7.2',
+        Underscore: 'libs/underscore'
+    }
+});
+
+require(['routing'], function (routing) {
+    routing.start();
+});
---
+++ src/Candidate.Nancy.Selfhosted/Client/scripts/routing.js
@@ -0,0 +1,30 @@
+﻿define(function (require) {
+
+    var Backbone = require('Backbone');
+    var ApplicationRouter = Backbone.Router.extend({
+        initialize: function () {
+
+        },
+
+        routes: {
+            '': 'home',
+            'account': 'account'
+        },
+
+        home: function () {
+            alert('home');
+        },
+
+        account: function () {
+            alert('account');
+        }
+
+    });
+
+    return {
+        start: function () {
+            new ApplicationRouter();
+            Backbone.history.start({pushState: true});
+        }
+    }
+});
